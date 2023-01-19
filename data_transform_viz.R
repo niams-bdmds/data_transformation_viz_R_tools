675e9744cbe344ef3397a35ac21b7c5e311e4f7b
Binod Regmi <regmibinod30@gmail.com>
2023-01-19 15:47:11 -0500

    function edited

--- data_transform_viz.R
+++ data_transform_viz.R
@@ -1,19 +1,19 @@
 # This script is version control form of git
 # This script is a collection of functions for data transformation
-log_transformed_scatter_plot <- function(df, X, Y, method = c("base10", "base2", "natural")){
+log_transformed_scatter_plot <- function(X, Y, method = c["base10", "base2", "natural"]){
     if (method =="base10" ){
-        df$log_transformed.X <- log(df$X)
-        df$long_transformed.Y <- log(df$Y)  
+        X_trans <- log10(X)
+        Y_trans <- log10(Y)  
     }else if(method =="base2"){
-        df$log_transformed.X <- log(df$X)
-        df$long_transformed.Y <- log(df$Y)  
+        X_trans <- log2(X)
+        Y_trans <- log2(Y) 
     }else if(method=="natural"){
-        df$log_transformed.X <- log(df$X)
-        df$long_transformed.Y <- log(df$Y)  
-    }else{
-        print("incorrect input parameters")
-    }
+        X_trans <- log(X)
+        Y_trans <- log(Y) 
+    }else if(method == "none"){
+    }else {print("Error: incorrect parameter input")}
    
-    # plotting
-    plot(df$log_transformed.X, df$long_transformed.Y, main="scatter plot of log transformed counts")
+   # plotting
+    plot(X_trans, Y_trans, col = "black", lwd = 4, main="scatter plot of log transformed counts")
 }
+
